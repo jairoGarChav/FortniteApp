0b2e6434cf5a16788defd3b94c9763ff4d922fb0
JairoGarChav <Jairogarcha@gmail.com>
2024-03-19 20:22:02 +0100

    a
    a

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORTINTE APP</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
-    <h1>ESTADÍSTICAS FORTNITE</h1>
-    <div class="tarjetas">
-        <div class="borde-partida-nueva">
-        <a href="formulario-partida-nueva.html">
-            <div class="tarjeta-partida-nueva">
-                
-                <img src="img/datos-logo.png" alt="Icono de una libreta y un bolígrafo">
-                <div class="tarjetas-texto">
-                    <h2>AÑADIR PARTIDA</h2>
-                    <p>Añade las estadísticas de la partida</p>
-                    
-                </div>
+        <h1 id="h1-inicio">ESTADÍSTICAS FORTNITE</h1>
+        <div class="tarjetas">
+            <div class="borde-partida-nueva">
+                <a href="formulario-partida-nueva.html">
+                    <div class="tarjeta-partida-nueva">
+                        <img src="img/datos-logo.png" alt="Icono de una libreta y un bolígrafo">
+                        <div class="tarjetas-texto">
+                            <h2>AÑADIR PARTIDA</h2>
+                            <p>Añade las estadísticas de la partida</p>
+                        </div>
+                    </div>
+                </a>
             </div>
-            </a>
-        </div>
-        <div class="borde-ver-ranking">
-            <div class="tarjeta-ver-ranking">
-                <img src="img/podio-logo.png" alt="Icono de una libreta y un bolígrafo">
-                <div class="tarjetas-texto">
-                    <h2>VER RANKING</h2>
-                    <p>Consulta las estadísticas de tus partidas</p>
-                </div>
+            <div class="borde-ver-ranking">
+                <a href="ranking-general.html">
+                    <div class="tarjeta-ver-ranking">
+                        <img src="img/podio-logo.png" alt="Icono de una libreta y un bolígrafo">
+                        <div class="tarjetas-texto">
+                            <h2>VER RANKING</h2>
+                            <p>Consulta las estadísticas de tus partidas</p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
-    </div>
     </div>
 </body>
+
 </html>
